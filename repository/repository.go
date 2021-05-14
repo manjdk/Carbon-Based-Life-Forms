dbd9a5652f0222dbc6dab64cb691b59218c0921a
Mantas <manjdk@telesoftas.com>
2021-05-15 00:55:22 +0300

    feat: Add repository implementation

---
+++ repository/repository.go
@@ -0,0 +1,50 @@
+package repository
+
+import (
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws/session"
+	"github.com/aws/aws-sdk-go/service/dynamodb"
+	"github.com/manjdk/Carbon-Based-Life-Forms/config"
+	"github.com/manjdk/Carbon-Based-Life-Forms/domain"
+)
+
+const (
+	keyID = "id"
+)
+
+type MineralCreateIFace interface {
+	Create(mineral *domain.Mineral) error
+}
+
+type MineralsGetIFace interface {
+	GetAll() ([]domain.Mineral, error)
+}
+
+type MineralGetByIDIFace interface {
+	Get(mineralID string) (*domain.Mineral, error)
+}
+
+type MineralUpdateStateIFace interface {
+	Update(mineral *domain.Mineral) error
+}
+
+type DynamoDB struct {
+	db           *dynamodb.DynamoDB
+	MineralTable string
+}
+
+func NewDynamoDB(config *config.Config) (*DynamoDB, error) {
+	awsConfig := aws.NewConfig().
+		WithRegion(config.AwsRegion).
+		WithEndpoint(config.DatabaseHost)
+
+	awsSession, err := session.NewSession(awsConfig)
+	if err != nil {
+		return nil, err
+	}
+
+	return &DynamoDB{
+		db:           dynamodb.New(awsSession),
+		MineralTable: config.MineralTable,
+	}, nil
+}
