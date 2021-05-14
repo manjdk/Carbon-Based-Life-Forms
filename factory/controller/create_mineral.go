1d842c2a5028ada1058698db6c8071aaa369e2c7
Mantas <manjdk@telesoftas.com>
2021-05-15 01:01:20 +0300

    feat: Add factory client with endpoints

---
+++ factory/controller/create_mineral.go
@@ -0,0 +1,33 @@
+package controller
+
+import (
+	"encoding/json"
+	"net/http"
+
+	"github.com/manjdk/Carbon-Based-Life-Forms/custom_http"
+	"github.com/manjdk/Carbon-Based-Life-Forms/domain"
+	"github.com/manjdk/Carbon-Based-Life-Forms/domain/usecase"
+	"github.com/manjdk/Carbon-Based-Life-Forms/error"
+	"github.com/manjdk/Carbon-Based-Life-Forms/log"
+)
+
+func CreateMineralFactory(createUC usecase.CreateMineralUC) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		traceID := r.Header.Get(custom_http.HeaderXTraceID)
+
+		mineral := new(domain.Mineral)
+		if err := json.NewDecoder(r.Body).Decode(mineral); err != nil {
+			log.ErrorZ(traceID, err).Msg("Failed to decode mineral create request in factory")
+			custom_http.NewResponse(w, http.StatusBadRequest, error.NewErrorMessage(err))
+			return
+		}
+
+		if err := createUC.Create(mineral); err != nil {
+			log.ErrorZ(traceID, err).Msg("Failed to create mineral")
+			custom_http.NewResponse(w, http.StatusInternalServerError, error.NewErrorMessage(err))
+			return
+		}
+
+		custom_http.NewResponse(w, http.StatusCreated, nil)
+	}
+}
---
+++ factory/main.go
@@ -0,0 +1,59 @@
+package main
+
+import (
+	"net/http"
+
+	"github.com/gorilla/mux"
+	"github.com/manjdk/Carbon-Based-Life-Forms/config"
+	"github.com/manjdk/Carbon-Based-Life-Forms/domain/usecase"
+	"github.com/manjdk/Carbon-Based-Life-Forms/factory/controller"
+	"github.com/manjdk/Carbon-Based-Life-Forms/log"
+	"github.com/manjdk/Carbon-Based-Life-Forms/queue"
+	"github.com/manjdk/Carbon-Based-Life-Forms/repository"
+)
+
+func main() {
+	cfg, err := config.NewConfig("config")
+	if err != nil {
+		log.FatalZ(err).Msg("Failed to initiate factory config")
+	}
+
+	db, err := repository.NewDynamoDB(cfg)
+	if err != nil {
+		log.FatalZ(err).Msg("Failed to create database")
+	}
+
+	factorySQSClient, err := queue.NewSQSClient(
+		cfg.QueueFactory.Name,
+		cfg.QueueFactory.Host,
+		cfg.AwsRegion,
+	)
+	if err != nil {
+		log.FatalZ(err).Msg("Failed to create factory SQS")
+	}
+
+	createMineralUC := usecase.NewCreateMineralUC(db)
+	getMineralByIDUC := usecase.NewGetMineralUC(db)
+	getAllMineralsUC := usecase.NewGetAllMineralUC(db)
+	meltMineralUC := usecase.NewMeltMineralUC(db, db)
+	condenseMineralUC := usecase.NewCondenseMineralUC(db, db)
+	fractureMineralUC := usecase.NewFractureMineralUC(db, db)
+
+	router := mux.NewRouter().StrictSlash(true)
+
+	router.HandleFunc("/minerals", controller.CreateMineralFactory(createMineralUC)).Methods(http.MethodPost)
+	router.HandleFunc("/minerals", controller.GetMineralsFactory(getAllMineralsUC)).Methods(http.MethodGet)
+	router.HandleFunc("/minerals/{mineralId}", controller.GetMineralFactory(getMineralByIDUC)).Methods(http.MethodGet)
+
+	go factorySQSClient.RunFactoryConsumer(
+		meltMineralUC,
+		condenseMineralUC,
+		fractureMineralUC,
+	)
+
+	log.InfoZ("NoTraceID").Msg("Factory is running")
+
+	if err := http.ListenAndServe(":8383", router); err != nil {
+		log.FatalZ(err).Msg("Failed to run factory client")
+	}
+}
