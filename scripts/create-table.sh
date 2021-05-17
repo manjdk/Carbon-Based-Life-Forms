a75aebfb966b5d8e20c939eda031ee3efdd05ef9
Mantas <manjdk@telesoftas.com>
2021-05-17 21:06:32 +0300

    feat: Add clientId database index

--- scripts/create-table.sh
+++ scripts/create-table.sh
@@ -17,8 +17,23 @@ then
     --table-name ${MINERAL_TABLE} \
     --attribute-definitions \
         AttributeName=id,AttributeType=S \
+        AttributeName=clientId,AttributeType=S \
     --key-schema \
         AttributeName=id,KeyType=HASH \
     --provisioned-throughput ReadCapacityUnits=10,WriteCapacityUnits=10 \
+    --global-secondary-indexes '[
+   {
+        "IndexName": "clientId-index",
+        "KeySchema": [
+            {"AttributeName": "clientId", "KeyType": "HASH"}
+         ],
+        "Projection": {
+            "ProjectionType": "ALL"
+        },
+        "ProvisionedThroughput": {
+            "ReadCapacityUnits": 10,"WriteCapacityUnits": 10
+        }
+   }
+]'
 
 fi
--- scripts/delete-table.sh
+++ scripts/delete-table.sh
@@ -7,8 +7,3 @@ aws dynamodb delete-table \
 --region eu-west-1 \
 --endpoint-url ${ENDPOINT_URL} \
 --table-name ${MINERAL_TABLE}
-
-aws dynamodb delete-table \
---region eu-west-1 \
---endpoint-url ${ENDPOINT_URL} \
---table-name ${MINERAL_TABLE}
